dbddcd2aff67a6c379edfdc1249aa5382e8d5488
tsteelematc <tsteele@madisoncollege.edu>
2025-03-02 16:47:11 -0600

    Considering fake_weather_data for data-viz week-07, hmm...

--- gen_data.py
+++ gen_data.py
@@ -1,18 +1,23 @@
 import csv
 from faker import Faker
+import random
 
 fake = Faker()
 
-with open("fake_login_data.csv", "w", newline="") as csvfile:
-    fieldnames = ["Name", "Date", "Country"]
+with open("fake_weather_data.csv", "w", newline="") as csvfile:
+    # fieldnames = ["Name", "Date", "Country"]
+    fieldnames = ["Date", "Country", "Weather"]
+    
+    weather_conditions = ['Sunny', 'Cloudy', 'Rainy', 'Snowy', 'Windy', 'Stormy']
+
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
     for _ in range(5000):
         writer.writerow(
             {
-                "Name": fake.name(),
                 "Date": fake.date_between(start_date="-90d", end_date="today"),
                 "Country": fake.country(),
+                "Weather": random.choice(weather_conditions)
             }
         )
